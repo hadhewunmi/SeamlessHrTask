bd8d71157b565881f1254f875c49923ee1540b88
Tosin Ogunkoyode <tosinogunkoyode@Tosins-MacBook-Pro.local>
2023-01-27 12:51:02 +0100

    Login and invalid script

---
+++ src/Logintest/InvalidLogin.java
@@ -0,0 +1,5 @@
+package Logintest;
+
+public class InvalidLogin {
+
+}
---
+++ src/Logintest/ValidLogin.java
@@ -0,0 +1,10 @@
+package Logintest;
+
+public class ValidLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
